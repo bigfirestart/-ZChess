ba5bfbd74476a23aa57fcbb5830d49496d11afe6
Timur <artur.halisov@mail.ru>
2018-04-10 13:38:32 +0300

    Primitives added

--- ZChess/ZChess/Board.cpp
+++ ZChess/ZChess/Board.cpp
@@ -8,15 +8,15 @@ using namespace std;
 Board::Board(){
 
 	Primitives::Figure king = { 'k','o',' ',{ { 1,1 },{ 1,0 },{ 0,1 },{ -1,1 },{ 1,-1 },{ 0,-1 },{ -1,0 },{ -1,-1 } },0,0 };
-	Primitives::Figure queen = {};
-	Primitives::Figure bishop;
-	Primitives::Figure horse;
+	Primitives::Figure queen = { 'q','f',' ',{ { 1,1 },{ 1,0 },{ 0,1 },{ -1,1 },{ 1,-1 },{ 0,-1 },{ -1,0 },{ -1,-1 } },0,0 };
+	Primitives::Figure bishop = { 'b','f',' ',{ { 1,1 },{ -1,1 },{ 1,-1 },{ -1,-1 } },0,0 };
+	Primitives::Figure horse = { 'b','f',' ',{ { 2,1 },{ 1,2 },{ -1,2 },{ -2,1 },{ -2,-1 },{ -1,-2 },{ 1,-2 },{ 2,-1 } },0,0 };
 	Primitives::Figure rook = { 'r','f',' ',{ { 1,0 },{ 0,1 },{ -1,0 },{ 0,-1 } } };
 	Primitives::Figure pawn = { 'p','d',' ',{ 1,0 } };
 
-	//matrix[0][0]  = { 'r','f','b',{ { 1,0 },{ 0,1 },{ -1,0 },{ 0,-1 } }};
+	matrix[0][0]  = rook;
 	matrix[0][1] = pawn;
-
+	//matrix[][]
 
 	
 
