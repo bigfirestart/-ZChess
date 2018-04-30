2820d3174eb76ac6ddeaa84d1c1bb52faa609618
bigfirestart <bigfirestart@gmail.com>
2018-04-30 12:32:35 +0300

    turn is added

--- ZChess/Board.h
+++ ZChess/Board.h
@@ -10,11 +10,12 @@ public:
 	bool Move(char l1, int n1 , char l2, int n2 );
 	Board();
 	~Board();
+	static int* Convert(char l, int n);
 
 private:
 	bool AnyObstacle(int _StartPos[2], int _FinalPos[2]);
 	void Fill();
-	static int* Convert(char l, int n);
+	
 	char* Convert_rev(int l, int n);
 };
 
--- ZChess/Game.cpp
+++ ZChess/Game.cpp
@@ -5,24 +5,51 @@
 
 using namespace std;
 void Game::start() {
-
+	//cout << "alalala";
 
 	Board board;
 
-
 	int n1, n2;
 	char l1, l2;
+	Player isTurnof = white_Player;
 
 	while (true) {
+		if (isTurnof.color == 'w') {
+			cout << "         white player turn"  << endl << "---------------------------------" <<endl;
+		}
+		else {
+			cout << "         black player turn" << endl <<  "----------------------------------" << endl;
+		}
 		board.Show();
 		cin >> l1 >> n1 >> l2 >> n2;
-		board.Move(l1, n1, l2, n2);
-		system("cls");
+		int xpos = Board::Convert(l1, n1)[0];
+		int ypos = Board::Convert(l1, n1)[1];
+
+		Primitives *thisFigure;
+		thisFigure = board.matrix[xpos][ypos];
+		char color = thisFigure->GetColor();
+		if (color != isTurnof.color) {
+			cout << "its not your turn" << endl;
+			system("pause");
+			system("cls");
+		}
+		else {
+			board.Move(l1, n1, l2, n2);
+			system("cls");
+			if (isTurnof.color == 'w') {
+				isTurnof = black_Player;
+			}
+			else {
+				isTurnof = white_Player;
+			}
+		}
 
 	}
 }
 Game::Game(){
 	white_Player.controlBy = "player";
 	black_Player.controlBy = "player";
+	white_Player.color = 'w';
+	black_Player.color = 'b';
 
 }
